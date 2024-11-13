f19c275016c104941fd6a8f6125e4ac5e277b8a7
yzoe76300 <zyangcg@connect.ust.hk>
2024-11-13 16:36:36 +0800

    开发短链接系统通用持久层实体填充对象

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/config/MyMetaObjectHandler.java
@@ -0,0 +1,25 @@
+package com.nageoffer.shortlink.admin.config;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+@Slf4j
+@Component
+public class MyMetaObjectHandler  implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        log.info("开始插入填充...");
+        this.strictInsertFill(metaObject, "createTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+        this.strictInsertFill(metaObject, "delFlag", Integer.class, 0);
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        log.info("开始更新填充...");
+        this.strictUpdateFill(metaObject, "updateTime", LocalDateTime.class, LocalDateTime.now());
+    }
+}
