5ca257becb678ae8b4756ffc0765464f1dd0ca68
yzoe76300 <zyangcg@connect.ust.hk>
2024-11-13 17:04:28 +0800

    解决用户注册时的毫秒级高并发问题

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.nageoffer.shortlink.admin.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.nageoffer.shortlink.admin.common.constant.RedisCacheConstant;
 import com.nageoffer.shortlink.admin.common.convension.exception.ClientException;
 import com.nageoffer.shortlink.admin.dao.entity.UserDO;
 import com.nageoffer.shortlink.admin.dao.mapper.UserMapper;
@@ -11,6 +12,8 @@ import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
 import com.nageoffer.shortlink.admin.service.IUserService;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBloomFilter;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -22,6 +25,8 @@ import static com.nageoffer.shortlink.admin.common.enums.UserErrorCodeEnum.USER_
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements IUserService {
     // 因爲交給spring管理所以多綫程狀態下注入的是同一個bean（單例bean）
     private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+    private final RedissonClient redissonClient;
+    
     @Override
     public UserRespDTO getUserByUsername(String username){
     /*
@@ -50,12 +55,24 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         if (this.hasUsername(requestParam.getUsername())) {
             throw new ClientException(USER_NAME_EXIST);
         }
-        UserDO userDO = new UserDO();
-        BeanUtils.copyProperties(requestParam, userDO);
-        int insert = baseMapper.insert(userDO);
-        if(insert < 1){
-            throw new ClientException(USER_NULL);
+        RLock lock = redissonClient.getLock(RedisCacheConstant.LOCK_USER_REGISTER_KEY +
+                                            requestParam.getUsername());
+        try {
+            if (lock.tryLock()) {
+                UserDO userDO = new UserDO();
+                BeanUtils.copyProperties(requestParam, userDO);
+                int insert = baseMapper.insert(userDO);
+                if(insert < 1){
+                    throw new ClientException(USER_NULL);
+                }
+                userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+                return;
+            }
+            throw new ClientException(USER_NAME_EXIST);
+        } finally {
+            lock.unlock();
         }
-        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
+
+
     }
 }
