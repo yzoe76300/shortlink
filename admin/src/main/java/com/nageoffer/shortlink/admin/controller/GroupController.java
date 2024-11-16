839bebdd09b270cf4cd0541268bed3a4a13dd2fd
yzoe76300 <zyangcg@connect.ust.hk>
2024-11-16 10:50:08 +0800

    feature: 短链接分组绑定用户上下文

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/GroupController.java
@@ -3,11 +3,15 @@ package com.nageoffer.shortlink.admin.controller;
 import com.nageoffer.shortlink.admin.common.convension.result.Result;
 import com.nageoffer.shortlink.admin.common.convension.result.Results;
 import com.nageoffer.shortlink.admin.dto.req.ShortLinkGroupSaveReqDTO;
+import com.nageoffer.shortlink.admin.dto.resp.ShortlinkGroupResponseDTO;
 import com.nageoffer.shortlink.admin.service.IGroupService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /*
 * 短链接分组控制层
@@ -17,10 +21,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class GroupController {
     private final IGroupService groupService;
 
+    /**
+     * 新增短链接分组
+     * @param requestParam
+     * @return
+     */
     @PostMapping("/api/short-link/v1/group")
     public Result<Void> save(@RequestBody ShortLinkGroupSaveReqDTO requestParam){
         groupService.saveGroup(requestParam.getName());
         return Results.success();
 
+    }
+
+    @GetMapping("/api/short-link/v1/group")
+    public Result<List<ShortlinkGroupResponseDTO>> groupList(){
+        List<ShortlinkGroupResponseDTO> groupList = groupService.getGroupList();
+        return Results.success(groupList);
     }
 }
--- admin/src/main/java/com/nageoffer/shortlink/admin/service/IGroupService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/IGroupService.java
@@ -2,6 +2,9 @@ package com.nageoffer.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nageoffer.shortlink.admin.dao.entity.GroupDO;
+import com.nageoffer.shortlink.admin.dto.resp.ShortlinkGroupResponseDTO;
+
+import java.util.List;
 
 /**
  * 短链接分组接口层
@@ -11,4 +14,6 @@ public interface IGroupService extends IService<GroupDO> {
     * 新增短链接分组
     * */
     public void saveGroup(String groupName);
+
+    List<ShortlinkGroupResponseDTO> getGroupList();
 }
