75ff052250101b3cc5316f1960e9165670eb1f27
yzoe76300 <zyangcg@connect.ust.hk>
2024-11-20 14:20:35 +0800

    feature: 短链接后台管理系统调用相关接口

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/ShortLinkController.java
@@ -0,0 +1,37 @@
+package com.nageoffer.shortlink.admin.controller;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.nageoffer.shortlink.admin.common.convension.result.Result;
+import com.nageoffer.shortlink.admin.remote.dto.ShortLinkRemoteService;
+import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkCreateReqDTO;
+import com.nageoffer.shortlink.admin.remote.dto.req.ShortLinkPageReqDTO;
+import com.nageoffer.shortlink.admin.remote.dto.resp.ShortLinkCreateRespDTO;
+import com.nageoffer.shortlink.admin.remote.dto.resp.ShortLinkPageRespDTO;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ShortLinkController {
+    private ShortLinkRemoteService service = new ShortLinkRemoteService(){};
+    /**
+     * 远程创建短链分页数据
+     * @param requestParam
+     * @return
+     */
+    @PostMapping("/api/short-link/admin/v1/create")
+    public Result<ShortLinkCreateRespDTO> createShortLink(@RequestBody ShortLinkCreateReqDTO requestParam) {
+        return service.createShortLink(requestParam);
+    }
+
+    /*
+     * 远程查询短链分页数据
+     * */
+    @GetMapping("/api/short-link/admin/v1/page")
+    public Result<IPage<ShortLinkPageRespDTO>> pageShortLink(ShortLinkPageReqDTO requestParam) {
+        return service.pageShortLink(requestParam);
+    }
+
+
+}
---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/remote/dto/resp/ShortLinkPageRespDTO.java
@@ -0,0 +1,70 @@
+package com.nageoffer.shortlink.admin.remote.dto.resp;
+
+import cn.hutool.db.Page;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.util.Date;
+
+/*
+* 短链接分页返回DTO
+* */
+
+@Data
+public class ShortLinkPageRespDTO extends Page {
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * domain
+     */
+    private String domain;
+
+    /**
+     * short_uri
+     */
+    private String shortUri;
+
+    /**
+     * full_short_url
+     */
+    private String fullShortUrl;
+
+    /**
+     * origin_url
+     */
+    private String originUrl;
+
+    /**
+     * gid
+     */
+    private String gid;
+
+    /**
+     * valid_date_type
+     */
+    private Integer validDateType;
+
+    /**
+     * valid_date
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date validDate;
+
+    /*
+    * 创建时间
+    * */
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    private Date createTime;
+    /**
+     * describe
+     */
+    private String describe;
+
+    /*
+    *  网站标识
+    * */
+    private String favicon;
+}
