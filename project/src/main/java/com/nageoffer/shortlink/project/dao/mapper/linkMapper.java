c7c4f9d1790421628506b907ebf6de643087a756
yzoe76300 <zyangcg@connect.ust.hk>
2024-12-01 18:21:36 +0800

    feature: 短链接监控汇总统计开发，未完成版本

--- project/src/main/java/com/nageoffer/shortlink/project/dao/mapper/linkMapper.java
+++ project/src/main/java/com/nageoffer/shortlink/project/dao/mapper/linkMapper.java
@@ -2,10 +2,15 @@ package com.nageoffer.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.nageoffer.shortlink.project.dao.entity.LinkDO;
+import org.apache.ibatis.annotations.Param;
 
 /*
 * 短链接持久层
 * */
 public interface linkMapper extends BaseMapper<LinkDO> {
-
+    void incrementStats(@Param("gid") String gid,
+                        @Param("fullShortUrl") String fullShortUrl,
+                        @Param("totalPv") Integer totalPv,
+                        @Param("totalUv") Integer totalUv,
+                        @Param("totalUip") Integer totalUip);
 }
