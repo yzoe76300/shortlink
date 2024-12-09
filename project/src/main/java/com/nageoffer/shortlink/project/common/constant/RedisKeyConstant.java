ec418229945704f7e1a9d4508b71c9ad1b1bcc8d
yzoe76300 <zyangcg@connect.ust.hk>
2024-12-09 11:11:03 +0800

    feature: 短链接监控消费幂处理

--- project/src/main/java/com/nageoffer/shortlink/project/common/constant/RedisKeyConstant.java
+++ project/src/main/java/com/nageoffer/shortlink/project/common/constant/RedisKeyConstant.java
@@ -18,4 +18,19 @@ public class RedisKeyConstant {
      *  短连链接跳转前缀key
      * */
     public static final String GOTO_IS_NULL_SHORT_LINK_KEY = "short-link_goto_%s";
+
+    /**
+     * 短链接监控消息保存队列 Topic 缓存标识
+     */
+    public static final String SHORT_LINK_STATS_STREAM_TOPIC_KEY = "short-link:stats-stream";
+
+    /**
+     * 短链接监控消息保存队列 Group 缓存标识
+     */
+    public static final String SHORT_LINK_STATS_STREAM_GROUP_KEY = "short-link:stats-stream:only-group";
+    /**
+     * 短链接监控消息保存队列幂等性校验标识
+     */
+    public static final String IDEMPOTENT_KEY_PREFIX = "short-link:idempotent:";
+
 }
